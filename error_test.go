43db3d411cc53830e8e094ef766226989dfd0941
Brian McGee <brian@41north.dev>
2022-09-08 15:33:05 +0200

    feat: add error tests

---
+++ error_test.go
@@ -0,0 +1,39 @@
+package jsonrpc
+
+import (
+	"encoding/json"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+var dataBytes, _ = json.Marshal("Some data")
+
+var testCases = []struct {
+	value any
+	json  string
+}{
+	{ErrInvalidRequest, "{\"code\":-32600,\"message\":\"invalid request\"}"},
+	{ErrMethodNotFound, "{\"code\":-32601,\"message\":\"method not found\"}"},
+	{ErrInvalidParams, "{\"code\":-32602,\"message\":\"invalid params\"}"},
+	{ErrInternal, "{\"code\":-32603,\"message\":\"internal error\"}"},
+	{ErrParse, "{\"code\":-32700,\"message\":\"parse error\"}"},
+	{Error{123, "error with data", dataBytes}, "{\"code\":123,\"message\":\"error with data\",\"data\":\"Some data\"}"},
+}
+
+func TestError_Marshal(t *testing.T) {
+	for _, tt := range testCases {
+		bytes, err := json.Marshal(tt.value)
+		assert.Nil(t, err, "failed to marshal to json")
+		assert.Equal(t, tt.json, string(bytes))
+	}
+}
+
+func TestError_Unmarshal(t *testing.T) {
+	for _, tt := range testCases {
+		var e Error
+		err := json.Unmarshal([]byte(tt.json), &e)
+		assert.Nil(t, err, "failed to unmarshal from json")
+		assert.Equal(t, tt.value, e)
+	}
+}
--- go.mod
+++ go.mod
@@ -9,7 +9,13 @@ require (
 	github.com/gorilla/websocket v1.5.0
 	github.com/juju/errors v1.0.0
 	github.com/sirupsen/logrus v1.9.0
+	github.com/stretchr/testify v1.8.0
 	golang.org/x/sync v0.0.0-20220907140024-f12130a52804
 )
 
-require golang.org/x/sys v0.0.0-20220825204002-c680a09ffe64 // indirect
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	golang.org/x/sys v0.0.0-20220825204002-c680a09ffe64 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
