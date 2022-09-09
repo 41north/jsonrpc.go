40cdf1a3de72e4fe4327379b034fd0dc101c0cad
Brian McGee <brian@41north.dev>
2022-09-09 12:41:24 +0200

    feat: refine Request and add tests

---
+++ request_test.go
@@ -0,0 +1,46 @@
+package jsonrpc
+
+import (
+	"encoding/json"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+var requestTestCases = []struct {
+	value Request
+	json  string
+}{
+	{
+		newRequest("ping", "foo"),
+		"{\"method\":\"ping\",\"params\":\"foo\",\"jsonrpc\":\"2.0\"}",
+	},
+	{
+		newRequest("pong", "bar", RequestNumericId(1456)),
+		"{\"id\":1456,\"method\":\"pong\",\"params\":\"bar\",\"jsonrpc\":\"2.0\"}",
+	},
+	{
+		newRequest("ping", nil, RequestStringId("req-1")),
+		"{\"id\":\"req-1\",\"method\":\"ping\",\"jsonrpc\":\"2.0\"}",
+	},
+	{
+		newRequest("pong", []string{"hello", "world"}, RequestNumericId(554), RequestVersion("1.0")),
+		"{\"id\":554,\"method\":\"pong\",\"params\":[\"hello\",\"world\"],\"jsonrpc\":\"1.0\"}",
+	},
+}
+
+func TestRequest_MarshalJSON(t *testing.T) {
+	for _, tc := range requestTestCases {
+		bytes, err := json.Marshal(tc.value)
+		assert.Nil(t, err, "failed to marshal to json")
+		assert.Equal(t, tc.json, string(bytes))
+	}
+}
+
+func TestRequest_UnmarshalJSON(t *testing.T) {
+	for _, tc := range requestTestCases {
+		r, err := RequestFromJSON([]byte(tc.json))
+		assert.Nil(t, err, "failed to unmarshal from json")
+		assert.Equal(t, tc.value, r)
+	}
+}
