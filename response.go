c4ae392a1f6959cac3da1a4a2a2e10ac3f512f47
Brian McGee <brian@41north.dev>
2022-09-09 13:34:08 +0200

    feat: refine Response and add tests

--- response.go
+++ response.go
@@ -6,34 +6,161 @@ import (
 	"github.com/juju/errors"
 )
 
-type Response struct {
+func ResponseStringId(id string) ResponseOption {
+	return func(opts *ResponseOptions) error {
+		opts.Id = id
+		return nil
+	}
+}
+
+func ResponseNumericId(id int) ResponseOption {
+	return func(opts *ResponseOptions) error {
+		opts.Id = int64(id)
+		return nil
+	}
+}
+
+func ResponseVersion(version string) ResponseOption {
+	return func(opts *ResponseOptions) error {
+		opts.Version = version
+		return nil
+	}
+}
+
+type ResponseOption = func(opts *ResponseOptions) error
+
+type ResponseOptions struct {
+	Id      any
+	Version string
+}
+
+func DefaultResponseOptions() ResponseOptions {
+	return ResponseOptions{
+		Version: "2.0",
+	}
+}
+
+func newResponse(result any, options ...ResponseOption) Response {
+	resp, err := NewResponse(result, options...)
+	if err != nil {
+		panic(err)
+	}
+	return resp
+}
+
+func NewResponse(result any, options ...ResponseOption) (Response, error) {
+	opts := DefaultResponseOptions()
+	for _, opt := range options {
+		if err := opt(&opts); err != nil {
+			return nil, err
+		}
+	}
+
+	resultBytes, err := json.Marshal(result)
+	if err != nil {
+		return nil, errors.Annotate(err, "failed to marshal result to json")
+	}
+
+	return &response{id: opts.Id, result: resultBytes, error: nil, jsonRpc: opts.Version}, nil
+}
+
+func newResponseError(error Error, options ...ResponseOption) Response {
+	resp, err := NewResponseError(error, options...)
+	if err != nil {
+		panic(err)
+	}
+	return resp
+}
+
+func NewResponseError(error Error, options ...ResponseOption) (Response, error) {
+	opts := DefaultResponseOptions()
+	for _, opt := range options {
+		if err := opt(&opts); err != nil {
+			return nil, err
+		}
+	}
+	return &response{id: opts.Id, result: nil, error: &error, jsonRpc: opts.Version}, nil
+}
+
+type Response interface {
+	Id() any
+	Result() json.RawMessage
+	UnmarshalResult(payload any) error
+	Error() *Error
+	JsonRpc() string
+}
+
+type response struct {
+	id      any
+	result  json.RawMessage
+	error   *Error
+	jsonRpc string
+}
+
+func (r *response) Id() any {
+	return r.id
+}
+
+func (r *response) Result() json.RawMessage {
+	return r.result
+}
+
+func (r *response) Error() *Error {
+	return r.error
+}
+
+func (r *response) JsonRpc() string {
+	return r.jsonRpc
+}
+
+func (r *response) UnmarshalResult(payload any) error {
+	if r.Error() != nil {
+		return r.Error()
+	}
+	return json.Unmarshal(r.Result(), payload)
+}
+
+func (r *response) MarshalJSON() ([]byte, error) {
+	var err error
+	var id json.RawMessage
+
+	if r.Id() != nil {
+		id, err = json.Marshal(r.Id())
+		if err != nil {
+			return nil, errors.Annotate(err, "failed to marshal id to json")
+		}
+	}
+
+	return json.Marshal(&responseDto{
+		Id:      id,
+		Result:  r.Result(),
+		Error:   r.Error(),
+		JsonRpc: r.JsonRpc(),
+	})
+}
+
+func ResponseFromJSON(data []byte) (Response, error) {
+	var dto responseDto
+	if err := json.Unmarshal(data, &dto); err != nil {
+		return nil, err
+	}
+
+	id, err := UnmarshalId(dto.Id)
+	if err != nil {
+		return nil, err
+	}
+
+	return &response{
+		id:      id,
+		result:  dto.Result,
+		error:   dto.Error,
+		jsonRpc: dto.JsonRpc,
+	}, nil
+}
+
+type responseDto struct {
 	Id      json.RawMessage `json:"id,omitempty"`
 	Result  json.RawMessage `json:"result,omitempty"`
 	Error   *Error          `json:"error,omitempty"`
 	JsonRpc string          `json:"jsonrpc"`
-}
-
-func (r *Response) UnmarshalId() (any, error) {
-	return unmarshalId(r.Id)
-}
-
-func (r *Response) UnmarshalResult(payload any) error {
-	if r.Error != nil {
-		return r.Error
-	}
-	return json.Unmarshal(r.Result, payload)
-}
-
-func unmarshalId(id json.RawMessage) (any, error) {
-	var key any
-	err := json.Unmarshal(id, &key)
-	if err != nil {
-		return nil, errors.Annotate(err, "failed to unmarshal id")
-	}
-	switch key.(type) {
-	case int, int8, int16, int32, int64, uint, uint8, uint16, uint32, uint64, string:
-		return key, nil
-	default:
-		return nil, errors.Errorf("id field must be an integer or a string, found: %v", key)
-	}
 }
